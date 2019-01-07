078e09ed536ecae5b358c07a820a844cb993e69b
Catherine Phillips <cphillips-19@peddie.org>
2019-01-07 17:15:56 -0500

    Added a robot project

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  public void robotInit() {
+
+  }
+
+  public void robotPeriodic() {
+  }
+
+  public void autonomousInit() {
+
+  }
+
+  public void autonomousPeriodic() {
+
+  }
+
+  public void teleopPeriodic() {
+  }
+
+  public void testPeriodic() {
+  }
+}
