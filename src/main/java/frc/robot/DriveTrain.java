50347788ef148826ed4f803b05c1e0f25ce7d9df
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2019-01-10 17:08:09 -0500

    Update DriveTrain.java

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,14 +1,39 @@
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import edu.wpi.first.wpilibj.Talon;
 
 public class DriveTrain extends Subsystem {
-  
+  private double leftSpeed;
+  private double rightSpeed;
 
-  @Override
+  private enum Mode_Types {
+    TELEOP
+  };
+
+  Mode_Types mode;
+  Talon rightMotor;
+  Talon leftMotor;
+
+  public DriveTrain() {
+    mode = Mode_Types.TELEOP;
+
+    rightMotor = new Talon(1);
+    leftMotor = new Talon(0);
+  }
+
+  public void arcadeDrive(double speed, double turn) {
+    leftSpeed = speed - turn;
+    rightSpeed = speed + turn;
+    mode = Mode_Types.TELEOP;
+  }
+
+  public void update() {
+    rightMotor.set(rightSpeed);
+    leftMotor.set(leftSpeed);
+  }
+
   public void initDefaultCommand() {
-   
+
   }
 }
