94f0679974dc2af5b65757fcdc2a52eb123d8540
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2019-01-10 16:42:50 -0500

    Update Robot.java
    updated robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,18 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
-
+  Joystick leftJoystick;
+  Joystick rightJoystick;
+  DriveTrain driveTrain;
+  
   public void robotInit() {
-
+    leftJoystick = new Joystick(0);
+    rightJoystick = new Joystick(1);
+    driveTrain = new DriveTrain();
   }
 
   public void robotPeriodic() {
@@ -20,6 +27,9 @@ public class Robot extends TimedRobot {
   }
 
   public void teleopPeriodic() {
+  
+    driveTrain.update();
+
   }
 
   public void testPeriodic() {
