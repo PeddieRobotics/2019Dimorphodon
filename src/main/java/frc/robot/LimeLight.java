9af8b0b96b7451a77328af31dca781ccd81f7363
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2019-01-11 16:42:59 -0500

    Update LimeLight.java
    first test of limelight, should get the dx and should output it

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -7,18 +7,39 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.sql.Driver;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 /**
  * Add your docs here.
+ * Here we connect to the Data that the lime light outputs in limeLightTable
+ * tx access the x offset from the crosshair 
  */
 public class LimeLight extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
+  NetworkTable limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = limeLightTable.getEntry("tx");
+
+  //don't know if we need this 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  /**
+  *Create a variable that stores our dx(x displacement) called x
+  * Report the x to the driverstation 
+  */
+  public void update(){
+    double x = tx.getDouble(0.0);
+    DriverStation.reportError("X displacement " + x, true);
+    
   }
 }
