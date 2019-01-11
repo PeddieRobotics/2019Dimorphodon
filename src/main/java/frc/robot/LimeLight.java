224f9958db05951d54f2429aa6fe93073ecd06d5
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2019-01-11 16:43:33 -0500

    Update LimeLight.java

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -7,18 +7,33 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.sql.Driver;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 /**
  * Add your docs here.
  */
 public class LimeLight extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+
+  NetworkTable limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = limeLightTable.getEntry("tx");
+
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  public void update(){
+    double x = tx.getDouble(0.0);
+    DriverStation.reportError("X displacement " + x, true);
+    
   }
 }
