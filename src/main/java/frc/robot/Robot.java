bbc792377a8b974af22b13b9136fde1ebe0c42dc
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2019-01-12 15:48:41 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,7 +29,7 @@ public class Robot extends TimedRobot {
   }
 
   public void teleopPeriodic() {
-
+    driveTrain.arcadeDrive(steeringWheel.getZ(), steeringWheel.getX());
     driveTrain.update();
 
   }
