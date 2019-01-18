ba7bce8dc98516c4314bbe1e1508127d75e63867
Sophia Phelan <32138411+SophiaPhelan@users.noreply.github.com>
2019-01-18 16:51:01 -0500

    Programmed outline of CargoIntake
    Added methods for intaking, ejecting, disabling, raising and lowering, and anything else I could before we actually get the robot.
    Also imported the phoenix json.

--- src/main/java/frc/robot/CargoIntake.java
+++ src/main/java/frc/robot/CargoIntake.java
@@ -1,14 +1,110 @@
 
 package frc.robot;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 public class CargoIntake extends Subsystem {
 
+  private static enum ModeType {INTAKING, EJECTING, HOLDING, DISABLED};
+  private ModeType mode = ModeType.DISABLED;
 
-  @Override
+  private CANSparkMax shoulder;
+  private CANSparkMax wrist;
+
+  private CANPIDController spid;
+  private CANPIDController wpid;
+
+  private boolean isDown;
+  private double ejectSpeed;
+  private double speed;
+
+//  private CANEncoder sEncoder;
+//  private CANEncoder wEncoder;
+
   public void initDefaultCommand() {
 
+    shoulder = new CANSparkMax( 0, MotorType.kBrushless );
+    wrist = new CANSparkMax( 1, MotorType.kBrushless );
+
+    spid = shoulder.getPIDController();
+    wpid = wrist.getPIDController();
+
+  //  sEncoder = shoulder.getEncoder();
+  //  wEncoder = wrist.getEncoder();
+
   }
+
+  public void intake() {
+    mode = ModeType.INTAKING;  //intakes
+  }
+
+  public void ejectFast() {
+    ejectSpeed = 1.0;
+    mode = ModeType.EJECTING;
+  }
+
+  public void ejectSlow() {
+    ejectSpeed = 0.5;
+    mode = ModeType.EJECTING;
+  }
+
+  public void disable() {
+    mode = ModeType.DISABLED;
+  }
+
+  public void clawUp() {
+    isDown = false;
+    mode = ModeType.HOLDING;
+  }
+
+  public void clawDown() {
+    isDown = true;
+    mode = ModeType.INTAKING;
+  }
+
+  public boolean hasCargo() {
+    //will use if we have a sensor
+  }
+
+  public void update(){
+		
+		switch(mode) {
+		
+		case INTAKING:
+		     
+		  speed = 1.0;
+		     
+		  if(hasCargo()) { //get rid off lasthasCargo
+			 	mode = ModeType.HOLDING; //once we have the cube, we prepare to hold and clamp
+       }
+       
+			break;
+		
+    case HOLDING:
+
+			speed = 0.001;
+    //	DriverStation.reportError("holding", false);
+    
+			break;
+		
+		case EJECTING:
+      speed = -ejectSpeed;
+			break; 
+			
+		case DISABLED:
+			speed = 0;
+			break;
+		}
+		
+		wrist.set(speed);
+		
+	}
+
 }
