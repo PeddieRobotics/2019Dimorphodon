cc0680ffbefb664248c9482a53c6defa1d3c35bf
Elizabeth Hu <ehu-21@peddie.org>
2019-01-23 15:20:16 -0500

    drivepid

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -31,8 +31,9 @@ public class DriveTrain {
 
   public static final double TURN_P = 0.02549; 
   public static final double TURN_I = 0.0;
-  private static final double DRIVE_KP = 0.15;
+  private static final double DRIVE_KP = 0.081;
   private static final double DRIVE_KI = 0.000001;
+  private static final double DRIVE_KD = 0.11;
   private static final double capSpeed = 0.5;
 
   // tracking
@@ -66,7 +67,7 @@ public class DriveTrain {
     drivePID = leftDriveMaster.getPIDController();
     drivePID.setP(DRIVE_KP);
     drivePID.setI(DRIVE_KI);
-    drivePID.setD(0.0);
+    drivePID.setD(DRIVE_KD);
     drivePID.setFF(0.0);
     drivePID.setOutputRange(-1.0, 1.0);
 
