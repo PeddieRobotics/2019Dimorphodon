861036c27f237f07ac5dc7c4c884f1713ba1613c
Svanik <34766427+Svanik@users.noreply.github.com>
2019-01-23 15:57:07 -0500

    Added Limelight get angle
    Added a method to get the angle to the target using Limelight.

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -17,9 +10,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
-/**
- * Add your docs here.
- */
 public class LimeLight extends Subsystem {
 
   NetworkTable limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
@@ -31,9 +21,9 @@ public class LimeLight extends Subsystem {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
-  public void update(){
-    double x = tx.getDouble(0.0);
-    DriverStation.reportError("X displacement " + x, true);
-    
+  public double getAngle(){
+    double angleToTarget = tx.getDouble(0.0);
+    DriverStation.reportError("Angle to target is: " + angleToTarget, true);
+    return(angleToTarget); 
   }
 }
