26c809edd8a39f911f704b38e38b273a720e412f
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2019-01-31 16:10:44 -0500

    Update Vision.java

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,11 +1,28 @@
 package frc.robot;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.first.wpilibj.CameraServer;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-
 public class Vision extends Subsystem {
-  
+  UsbCamera camera1;
+  UsbCamera camera2;
+
+  public Vision() {
+    camera1 = CameraServer.getInstance().startAutomaticCapture(0);
+    camera1.setResolution(640, 360);
+    camera1.setExposureManual(0);
+    camera1.setBrightness(50);
+    camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+    camera2.setResolution(640, 360);
+    camera2.setExposureAuto();
+    // camera2.setExposureManual(0);
+  }
+
   public void initDefaultCommand() {
-  
+
   }
 }
