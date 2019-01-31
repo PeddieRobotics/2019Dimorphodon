32027f6012d5f1d106e2748af4f58d05eeadc91d
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2019-01-31 16:16:16 -0500

    Update LimeLight.java
    Cleaned it up

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -11,8 +11,6 @@ public class LimeLight {
     NetworkTableEntry tx;
     NetworkTableEntry ty;
     NetworkTableEntry tv;
-    NetworkTableEntry cy0;
-    NetworkTableEntry cx0;
     final double mountAngle = 0.0;
     final double targetHeight = 28.75;
     final double mountHeight = 7.25;
@@ -24,9 +22,6 @@ public class LimeLight {
         tx = table.getEntry("tx");
         ty = table.getEntry("ty");
         tv = table.getEntry("tv");
-        cy0 = table.getEntry("cy0");
-        cx0 = table.getEntry("cx0");
-
     }
 
     /**
@@ -37,37 +32,18 @@ public class LimeLight {
         if (hasTarget == 1) {
             yAngle = ty.getDouble(0.0);
             xAngle = tx.getDouble(0.0);
-            SmartDashboard.putNumber("xAngle", xAngle);
-            SmartDashboard.putNumber("YDistance", calcYDist());
-            SmartDashboard.putNumber("yAngle", yAngle);
+            SmartDashboard.putNumber("tx", xAngle);
+            SmartDashboard.putNumber("Distance", calcDist());
+            SmartDashboard.putNumber("ty", yAngle);
             // SmartDashboard.putNumber("radYAngle", Math.toRadians(yAngle));
-            SmartDashboard.putNumber("XDistance", calcXDist());
         }
     }
 
-    /**
-     * Assuming we know the mount height, mount angle, and the height of the target,
-     * and we are pointed straight at the target Then our distance is the tangent of
-     * our targetHeight - mountHeight/yAngle + mountAngle In order to make this work
-     * we need to be alligned with the target so our x angle is 0
-     * 
-     * @return the distance to the target
-     */
-    public double calcYDist() {
+    public double calcDist() {
         double distance = (targetHeight - mountHeight) / Math.tan(Math.toRadians(yAngle));
         return distance;
     }
 
-    public double calcXDist() {
-        double distance = (calcYDist() * Math.tan(Math.toRadians(xAngle)));
-        return distance;
-    }
-
-    /**
-     * this angle is just our crosshair angle(i think)
-     * 
-     * @return
-     */
     public double calcAngle() {
         return xAngle;
     }
