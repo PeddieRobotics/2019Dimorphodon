032d71f84de84fd0e31e3026cafe15699cc96154
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2019-02-26 18:14:26 -0500

    Update LimeLight.java
    updated limelight with new methods

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -1,29 +1,42 @@
-package frc.robot;
+]package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.*;
+
+import java.util.ArrayList;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimeLight {
-
+    final double vertIterations = 0;
     NetworkTable table;
     NetworkTableEntry tx;
     NetworkTableEntry ty;
     NetworkTableEntry tv;
+    NetworkTableEntry tvert; 
     final double mountAngle = 0.0;
-    final double targetHeight = 28.75;
-    final double mountHeight = 4.5;
+    final double targetHeight = 29;
+    final double mountHeight = 32.75;
     double yAngle = 0;
     double xAngle = 0;
-
+    double verticalHeigth; 
+    double currentTvert = 0;
+    ArrayList<Double> averageTVert = new ArrayList<Double>();
     public LimeLight() {
         table = NetworkTableInstance.getDefault().getTable("limelight");
         tx = table.getEntry("tx");
         ty = table.getEntry("ty");
         tv = table.getEntry("tv");
+        
+        tvert = table.getEntry("tvert");
     }
-
+    public double tx(){
+        return this.xAngle;
+    }
+    public double ty(){
+        return this.yAngle;
+    }
     /**
      * Make sure we are constantly updating our network table values
      */
@@ -32,27 +45,45 @@ public class LimeLight {
         if (hasTarget == 1) {
             yAngle = ty.getDouble(0.0);
             xAngle = tx.getDouble(0.0);
+            currentTvert = tvert.getDouble(0.0);    
+            if(currentTvert>0){updateTVert(currentTvert);}
             SmartDashboard.putNumber("tx", xAngle);
             SmartDashboard.putNumber("Distance", calcDist());
             SmartDashboard.putNumber("ty", yAngle);
             // SmartDashboard.putNumber("radYAngle", Math.toRadians(yAngle));
         }
     }
+    public boolean hasTarget(){
+        return(tv.getDouble(0.0)==1);
+    }
 
     public double calcDist() {
         double distance = (targetHeight - mountHeight) / Math.tan(Math.toRadians(yAngle));
         return distance;
     }
-
+    public double calcXDist(){
+        double distance = calcDist()/Math.tan(Math.toRadians(xAngle));
+        return distance;
+    }
     public double calcAngle() {
         return xAngle;
     }
-
-    public void blink() {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(2);
+    public double toFeet(double inches){
+        return inches/12;
     }
-
-    public void solid() {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
+    public void updateTVert(double current){
+        averageTVert.add(current);
+        if(averageTVert.size()>vertIterations){
+            averageTVert.remove(0);
+        }
+    }
+    public double tvertAverage(){
+        double average = 0;
+        if(averageTVert.size()>0){
+            for(int i =0; i < averageTVert.size();i++){
+                average += averageTVert.get(i);
+            }
+        }
+        return average/averageTVert.size();
     }
 }
